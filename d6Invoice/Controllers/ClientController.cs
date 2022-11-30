f0cd3ca830da34873f030d1dedc060125b4057f5
HendrikOosthuizen <hoosthuizen@polysphere.co.za>
2022-11-30 15:52:09 +0200

    Create Controllers

---
+++ d6Invoice/Controllers/ClientController.cs
@@ -0,0 +1,89 @@
+﻿using d6Invoice.Models;
+using d6Invoice.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace d6Invoice.Controllers
+{
+  public class ClientController : Controller
+  {
+    private readonly AdoNet _net;
+
+    public ClientController()
+    {
+      _net = new AdoNet( ConfigurationManager.ConnectionStrings[ "DefaultConnectionString" ].ConnectionString );
+    }
+
+
+    //GET Client/index
+    public async Task< ActionResult > Index( int page, int recordsPerPage )
+    {
+      Hashtable             parameters = new Hashtable { { "@page", page }, { "@recPerPage", recordsPerPage } };
+      IEnumerable< Client > result     = await _net.StpAsync< Client >( "Client_Get", parameters );
+
+      return View( result );
+    }
+
+    //GET Client/details
+    public async Task< ActionResult > Details( int id )
+    {
+      Hashtable             parameters = new Hashtable { { "@Id", id } };
+      IEnumerable< Client > result     = await _net.StpAsync< Client >( "Client_GetDetails", parameters );
+
+      return View( result.First() );
+    }
+
+    //GET Client/create
+    public ActionResult Create() { return View(); }
+
+    //POST Client/create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task< ActionResult > Create( [Bind( Include = "Name, Address, Suburb, State, ZipCode" )] Client model )
+    {
+      if ( !ModelState.IsValid ) return View( model );
+
+      return RedirectToAction( "Index" );
+    }
+
+    //GET Client/edit
+    public async Task< ActionResult > Edit( int id )
+    {
+      Hashtable             parameters = new Hashtable { { "@Id", id } };
+      IEnumerable< Client > result     = await _net.StpAsync< Client >( "Client_GetDetails", parameters );
+
+      return View(result.First());
+    }
+
+    //POST Client/edit
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task< ActionResult > Edit(
+      [Bind( Include = "Id,Name, Address, Suburb, State, ZipCode" )]
+      Client model )
+    {
+      if ( !ModelState.IsValid ) return View( model );
+
+      return RedirectToAction( "Index" );
+    }
+
+    //GET Client/delete
+    public async Task< ActionResult > Delete( int id )
+    {
+      Hashtable             parameters = new Hashtable { { "@Id", id } };
+      IEnumerable< Client > result     = await _net.StpAsync< Client >( "Client_GetDetails", parameters );
+
+      return View(result.First());
+    }
+
+    //POST Client/delete
+    [HttpPost]
+    [ActionName( "Delete" )]
+    [ValidateAntiForgeryToken]
+    public async Task< ActionResult > DeleteConfirmed( int Id ) { return RedirectToAction( "Index" ); }
+  }
+}
--- d6Invoice/Models/Client.cs
+++ d6Invoice/Models/Client.cs
@@ -1,12 +1,26 @@
-﻿namespace d6Invoice.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace d6Invoice.Models
 {
   public class Client
   {
     public int    Id      { get; set; }
+
+    [Required]
+    [DisplayName("Name")]
     public string Name    { get; set; }
+
+    [DisplayName( "Address")]
     public string Address { get; set; }
+
+    [DisplayName( "Suburb")]
     public string Suburb  { get; set; }
+
+    [DisplayName( "State")]
     public string State   { get; set; }
+
+    [DisplayName( "Zip Code")]
     public string ZipCode { get; set; }
   }
 }
